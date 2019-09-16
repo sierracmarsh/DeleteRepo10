787e46c9384172c9a6c209649c28b27bbfe55e21
Sierra Marsh <10617333@my.uvu.edu>
2019-09-16 13:46:29 -0600

    Powerbooster

---
+++ Sophmore Work/Assets/Scripts/PowerBooster.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class PowerBooster : MonoBehaviour
+{
+    public float speed = 1;
+    public enum PowerStates
+    {
+        Normal,
+        Fast,
+        Slow,
+        Stop
+    }
+
+    public PowerStates powerState = PowerStates.Normal;
+
+    public void Normal()
+    {
+        speed = 1;
+    }
+
+    public void Fast()
+    {
+        speed = 5f;
+    }
+
+    public void Slow()
+    {
+        speed = .2f;
+    }
+
+    public void Stop()
+    {
+        speed = 0f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void Update(){
+        
+        
+            if (powerState == PowerStates.Normal)
+            {
+                Normal();
+                
+            }
+            else if (powerState == PowerStates.Fast)
+            {
+                Fast();
+                
+            }
+            else if (powerState == PowerStates.Slow)
+            {
+                Slow();
+            
+            }
+            else
+            {
+                Stop();
+               
+            }
+        var speedVector = new Vector3(x: speed, y:0, z:0)*Time.deltaTime;
+
+            transform.Translate(speedVector);
+        
+    }
+}
