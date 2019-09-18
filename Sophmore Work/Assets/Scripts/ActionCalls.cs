82ac50f80e5189b10614171ed75c6cc7e4922d21
Sierra Marsh <10617333@my.uvu.edu>
2019-09-18 14:41:04 -0600

    Action calls, kinda broke

---
+++ Sophmore Work/Assets/Scripts/ActionCalls.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionCalls : MonoBehaviour
+{
+    public float speed = 1f;
+    public float rotateSpeed = 2f;
+    public float scaleSpeed = .02f;
+    private Vector3 location;
+    private Vector3 rotations;
+    private Vector3 scales;
+    private  UnityAction TrasformAction;
+
+    public void Start()
+    {
+        TrasformAction = OnMove;
+    }
+
+    public enum TrasformStates
+
+    {
+        Move,
+        Rotate,
+        Scale,
+    }
+
+    public TrasformStates trasformstate;
+
+    private void Update()
+    {
+        TrasformAction();
+
+    }
+    
+
+    public void OnMove()
+        {
+            location.x = speed * Time.deltaTime;
+            transform.Translate(location);
+        }
+
+    public void OnRotate()
+    {
+        rotations.y = rotateSpeed * Time.deltaTime;
+        transform.Rotate(rotations);
+    }
+
+    public void OnScale()
+    {
+        scales.Set(scaleSpeed, scaleSpeed, scaleSpeed);
+        transform.localScale += scales;
+    }
+
+   
+}
