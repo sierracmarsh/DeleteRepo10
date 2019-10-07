82193b0ced489e4ce10949bddc119c1d446aa530
Sierra Marsh <10617333@my.uvu.edu>
2019-10-07 14:24:31 -0600

    Kings game, work on Collections

---
+++ Sophmore Work/Assets/Scripts/Collections.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Collections : ScriptableObject
+{
+   public List<Object> objectList;
+   public List<FloatData> FloatDatas;
+
+   public void AddToList(Object obj)
+   {
+      objectList.Add(obj);
+   }
+}
