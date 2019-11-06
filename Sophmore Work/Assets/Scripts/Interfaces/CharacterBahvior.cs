ce01823e25863f5dcd68fb2f6f29b645a88ca00b
Sierra Marsh <10617333@my.uvu.edu>
2019-11-06 14:06:07 -0700

    Scripts, Evolve
    Reminder: Turn into character movement variations

---
+++ Sophmore Work/Assets/Scripts/Interfaces/CharacterBahvior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBahvior : MonoBehaviour
+{
+    private WaitForFixedUpdate waitffu;
+    private bool canMove = true;
+
+    IEnumerator Start()
+    {
+        while (canMove)
+        {
+            Debug.Log();
+            yield return waitffu;
+        }
+    }
+}
