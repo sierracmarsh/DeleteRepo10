796ff85a653162d2ba556f3f3c660cde8ca658f7
Sierra Marsh <10617333@my.uvu.edu>
2019-11-11 15:32:03 -0700

    Character Adjustments

---
+++ Kings Game/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterBase : ScriptableObject
+{
+    public float moveSpeed = 3f;
+    public float gravity = 75f;
+    public float jumpSpeed = 75f;
+    
+    
+
+}
