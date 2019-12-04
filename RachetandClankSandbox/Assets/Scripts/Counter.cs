f10e6862a03875f94038ff2b78a460afb2348dd9
Sierra Marsh <10617333@my.uvu.edu>
2019-12-04 14:36:59 -0700

    Ship Animations

---
+++ RachetandClankSandbox/Assets/Scripts/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+	public Text counter;
+	private float startnumb;
+
+
+	void Start ()
+	{
+		startnumb = Time.time;
+	}
+	
+	
+	void Update ()
+	{
+		float c = Time.time - startnumb;
+		string minutes = ((int) c / 60).ToString();
+		string seconds = (c % 60).ToString();
+		counter.text = minutes + ":" + seconds;
+	}
+}
