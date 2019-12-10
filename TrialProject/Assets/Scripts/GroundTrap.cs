9a8910b7ac079aee65e55cbfe9b442508358b6a5
Sierra Masrh <10617333@my.uvu.edu>
2019-12-10 16:16:36 -0700

    Trial LEvels

---
+++ TrialProject/Assets/Scripts/GroundTrap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class GroundTrap: MonoBehaviour
+{
+	
+	private bool away = false;
+	private float awayspeed = 0;
+	
+
+	void OnTriggerEnter(Collider touch)
+	{
+		if (touch.gameObject.name == "MainCharacter_0")
+		{
+			away = true;
+			Destroy(gameObject, 10);
+		}
+	}
+
+	void Update()
+		{
+			if (away)
+			{
+				awayspeed += Time.deltaTime/20;
+				transform.position = new Vector3 (transform.position.x, transform.position.y - awayspeed,
+					transform.position.z);
+				
+			}
+		}
+		
+	}
+
+//scriptutorialStuartSpence
